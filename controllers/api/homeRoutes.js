bf6f21d434be51ba79f59d8c558947e15e522393
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-06-04 00:47:54 -0600

    added home route

---
+++ controllers/api/homeRoutes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Route to get homepage
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['content', 'user_id', 'date_created'],
+          include: [
+            User,
+          ],
+        },
+      ],
+    });
+
+const posts = postData.get({ plain: true });
+    res.render('comment', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
