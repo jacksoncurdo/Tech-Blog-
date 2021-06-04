7e911618dc85175b5bc1c45222986dd58f2c929c
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-06-04 13:50:15 -0600

    set up the server

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+// Setting up seqeulize 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Setting up express and setting the port to 3001
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -25,16 +26,12 @@ const sess = {
 };
 
 app.use(session(sess));
-
-// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
+app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
