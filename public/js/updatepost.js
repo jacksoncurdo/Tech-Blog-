17c6dab8fafe1071ef8ab8924546661932874993
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-06-04 11:40:02 -0600

    finished working on update.js

--- public/js/updatepost.js
+++ public/js/updatepost.js
@@ -1,2 +1,27 @@
 const editFormHandler = async (event) => {
     event.preventDefault();
+
+    const id = document.querySelector('#post-id').value.trim();
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+  
+    if (id && title && content) {
+      const response = await fetch(`/api/posts/update/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ id, title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/:id');
+      } else {
+        alert('Failed to update post');
+      }
+    }
+  };
+
+  document
+  .querySelector('.update-form')
+  .addEventListener('submit', editFormHandler);
